59207b083057d788aab1ad6bf8ddeda1e7d51b2d
Ademola <demisterling007@gmail.com>
2026-01-18 21:50:33 +0100

    added alx-react-app-new

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <>
+      <footer style={{ color: "grey" }}>
+        <p>© 2023 City Lovers</p>
+      </footer>
+    </>
+  );
+}
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+export default function MainContent() {
+  return (
+    <>
+      <main style={{ color: "grey" }}>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+    </>
+  );
+}
