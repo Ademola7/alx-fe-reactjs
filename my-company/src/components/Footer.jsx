5b701a523e370dc41219faa22fc0adcc43981215
Ademola <demisterling007@gmail.com>
2026-01-18 23:20:26 +0100

    added a new diectory called my-company

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        padding: "15px",
+        textAlign: "center",
+        backgroundColor: "#f2f2f2",
+        marginTop: "40px",
+      }}
+    >
+      <p>© 2026 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
