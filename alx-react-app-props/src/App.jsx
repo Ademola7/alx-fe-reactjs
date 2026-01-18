6a9049c5ba5438fb89f13a618869af70222dc77c
Ademola <demisterling007@gmail.com>
2026-01-18 23:47:34 +0100

    debugging error

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,15 +1,15 @@
-import UserProfile from "./UserProfile";
+import ProfilePage from "./ProfilePage";
 import UserContext from "./UserContext";
 
 function App() {
-  const user = {
+  const userData = {
     name: "Jane Doe",
     email: "jane.doe@example.com",
   };
 
   return (
-    <UserContext.Provider value={user}>
-      <UserProfile />
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
     </UserContext.Provider>
   );
 }
