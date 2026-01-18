31e79fb898b82de6097471deb999f51890ee2396
Ademola <demisterling007@gmail.com>
2026-01-19 00:27:54 +0100

    debugging error

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,17 +1,48 @@
-import ProfilePage from "./ProfilePage";
-import UserContext from "./UserContext";
-
+import "./App.css";
+import WelcomeMessage from "./components/WelcomeMessage.jsx";
+import Header from "./components/Header.jsx";
+import MainContent from "./components/MainContent.jsx";
+import Footer from "./components/Footer.jsx";
+import UserProfile from "./components/UserProfile.jsx";
+import ProfilePage from "./components/ProfilePage.jsx";
+import UserContext from "./components/UserContext.js";
 function App() {
   const userData = {
     name: "Jane Doe",
-    email: "jane.doe@example.com",
+    email: "janedoe.email.com",
   };
-
   return (
-    <UserContext.Provider value={userData}>
-      <ProfilePage />
-    </UserContext.Provider>
+    <div>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+      <UserContext.Provider value={userData}>
+        <ProfilePage />
+      </UserContext.Provider>
+      <UserProfile
+        name={"Alice"}
+        age={25}
+        bio={"Loves hiking and photography."}
+      />
+    </div>
   );
 }
 
-export default App;
+export default App; // import ProfilePage from "./ProfilePage";
+// import UserContext from "./UserContext";
+
+// function App() {
+//   const userData = {
+//     name: "Jane Doe",
+//     email: "jane.doe@example.com",
+//   };
+
+//   return (
+//     <UserContext.Provider value={userData}>
+//       <ProfilePage />
+//     </UserContext.Provider>
+//   );
+// }
+
+// export default App;
---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import UserContext from "./UserContext";
+
+const UserProfile = function (props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
